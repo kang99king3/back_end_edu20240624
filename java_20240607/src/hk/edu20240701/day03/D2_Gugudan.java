dfe0ebe194a4e68a0337a7706ca015cd51a1dc48
hankyung <hankyung@bjkang>
2024-07-01 14:25:27 +0900

    뱅크테스트 예제추가

--- java_20240607/src/hk/edu20240701/day03/D2_Gugudan.java
+++ java_20240607/src/hk/edu20240701/day03/D2_Gugudan.java
@@ -65,6 +65,22 @@ public class D2_Gugudan {
 		//실행결과 출력하기: (2,5) (1,3) (4,3).....
 		//1~6까지의 숫자로 구성되며, 프로그램상에서는 랜덤하게 숫자가 생성되도록 해야함
 		//Math 객체에 random()메서드를 사용하면 랜덤한 숫자를 얻을 수 있음
+		//수학관련 내용 기능 제공
+		//random(); 0.00000....1  ~ 0.999999  -> 0과 1사이의 실수를 반환
+		int number=(int)Math.random();
+		System.out.println("랜덤숫자생성:"+number+(Math.random()));
+		//우리가 필요한 숫자는 1~6
+		number=(int)(Math.random()*6+1);//  0~1사이  *6  1 *6= 6 
+		                                // 5.999999 +1 = 6.9999..
+		while(true) {
+			int num1=(int)(Math.random()*6+1);//주사위1번
+			int num2=(int)(Math.random()*6+1);//주사위2번
+			System.out.printf("(%d,%d)\n",num1,num2);
+			if(num1+num2==5) {
+				System.out.println("합이 5가되어 종료합니다.");
+				break;
+			}
+		}
 	}
 }
 
---
+++ java_20240607/src/hk/edu20240701/day03/D3_BankTest.java
@@ -0,0 +1,42 @@
+package hk.edu20240701.day03;
+
+import java.util.Scanner;
+
+public class D3_BankTest {
+
+	public static void main(String[] args) {
+		//클래스를 객체 생성해서 변수에 저장할때 선언되는 타입 --> 참조타입
+		Scanner scan=new Scanner(System.in);// System.in -> 키보드로 입력되는 정보
+		System.out.println("키보드로 입력하세요");
+		String s=scan.nextLine();//콘솔에서 입력되는 내용을 받기위해 대기중--> 입력하면 읽어들인다
+		System.out.println("입력된 내용:"+s);
+		
+		//예제
+		// while문과 Scanner객체를 이용해서 키보드로부터 입력된 데이터로 예금,출금,조회,종료 기능 구현
+		// 예금한 금액을 저장할 변수 선언
+		// 키보드로 입력받은 값을 저장할 변수 선언
+		
+		while(true) {
+			System.out.println("------------------------");
+			System.out.println("1.예금|2.출금|3.잔고|4.종료");
+			System.out.println("------------------------");
+			System.out.println("선택>");
+			
+			if() {
+				
+			}else if(){
+				
+			}else {
+				
+			}
+			
+		}
+		
+	}
+
+}
+
+
+
+
+
