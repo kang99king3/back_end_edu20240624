a6ec5f07a2146faced7c7c9ed017355ec54f9196
bjkang <kang99king3@gmail.com>
2024-10-08 14:55:41 +0900

    추가

--- 13_answerboard_springMVC/src/main/java/com/hk/board/controller/AnsController.java
+++ 13_answerboard_springMVC/src/main/java/com/hk/board/controller/AnsController.java
@@ -1,16 +1,41 @@
 package com.hk.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hk.board.dtos.AnswerDto;
+import com.hk.board.service.IAnsService;
 
 @Controller
 public class AnsController {
 
+	@Autowired
+	private IAnsService ansService;
+	
 	@RequestMapping(value = "/home.do"
 				  ,method = RequestMethod.GET)
 	public String home() {
 		System.out.println("HOME 페이지로 이동");
 		return "home";
 	}
+	
+	@RequestMapping(value = "/boardlist.do"
+			      ,method = RequestMethod.GET)
+	public String boardList(Model model, String pnum) {
+		
+		List<AnswerDto> list=ansService.getAllList(pnum);
+		model.addAttribute("list", list);
+		
+		return "boardlist";
+	}
 }
+
+
+
+
+
