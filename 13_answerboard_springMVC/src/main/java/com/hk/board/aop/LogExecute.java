d2983012b2ffabe68b8734aa51a91dcdad20d91e
bjkang <kang99king3@gmail.com>
2024-10-10 11:18:43 +0900

    log aop 추가

---
+++ 13_answerboard_springMVC/src/main/java/com/hk/board/aop/LogExecute.java
@@ -0,0 +1,14 @@
+package com.hk.board.aop;
+
+public class LogExecute {
+	/*
+	 * JoinPoint에서 제공하는 메서드
+	 * 		  - getTarget():대상 객체를 반환
+	 * 		  - getArgs():메서드의 아규먼트를 반환
+	 * 		  - toLongString():메서드의 모든 정보를 반환
+	 * 		  - getSignature():호출되는 메서드에 대한 정보를 담고 있는 객체 반환
+	 * 				-getName():메서드의 이름 구함
+	 *  			-toLongName():메서드의 풀네임(리턴타입, 파라미터타입 모두 표시)
+	 *  			-toShortName(): 메서드를 축약해서 표현(메서드의 이름나 표시)
+	 */
+}
