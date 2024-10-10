d919fd0e4fb9a26f52bba83a090fc251918f0709
bjkang <kang99king3@gmail.com>
2024-10-10 11:42:47 +0900

    추가

--- 13_answerboard_springMVC/src/main/java/com/hk/board/aop/LogExecute.java
+++ 13_answerboard_springMVC/src/main/java/com/hk/board/aop/LogExecute.java
@@ -1,5 +1,11 @@
 package com.hk.board.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LogExecute {
 	/*
 	 * JoinPoint에서 제공하는 메서드
@@ -11,4 +17,38 @@ public class LogExecute {
 	 *  			-toLongName():메서드의 풀네임(리턴타입, 파라미터타입 모두 표시)
 	 *  			-toShortName(): 메서드를 축약해서 표현(메서드의 이름나 표시)
 	 */
+	
+	//target 메서드가 실행되기전에 수행될 기능을 정의
+	public void before(JoinPoint join) {//target 메서드의 정보를 JoinPoint로 전달
+		Logger log=LoggerFactory.getLogger(join.getClass()+"");
+		log.info("before실행(info):시작:"+join.getSignature().getName());
+		log.debug("before실행(debug):시작:"+join.toLongString());
+	}
+	
+	//target메서드가 실행된 후 값을 성공적으로 리턴했다면 수행될 기능을 정의
+	public void afterReturning(JoinPoint join) {
+		Logger log=LoggerFactory.getLogger(join.getClass()+"");	
+		log.info("afterReturning실행(info):시작:"+join.getSignature().getName());
+		log.debug("afterReturning실행(debug):시작:"+join.toLongString());
+		Object[] args=join.getArgs();
+		log.debug("전달 파라미터:"+Arrays.toString(args));
+	}
+	
+	//target메서드에서 오류가 발생했을때 수행될 기능을 정의
+	public void daoError(JoinPoint join) {
+		Logger log=LoggerFactory.getLogger(join.getClass()+"");
+		log.info("daoError실행(info):시작:"+join.getSignature().getName());
+		log.debug("daoError실행(debug):시작:"+join.toLongString());
+		log.debug("debug실행:오류발생");
+	}
 }
+
+
+
+
+
+
+
+
+
+
