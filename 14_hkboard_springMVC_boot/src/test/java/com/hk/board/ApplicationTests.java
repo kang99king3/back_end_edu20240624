de37aa14e4758719f037ccfd9163b1ed764a5232
bjkang <kang99king3@gmail.com>
2024-10-14 15:03:06 +0900

    test 추가

--- 14_hkboard_springMVC_boot/src/test/java/com/hk/board/ApplicationTests.java
+++ 14_hkboard_springMVC_boot/src/test/java/com/hk/board/ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.hk.board;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hk.board.dtos.HkDto;
+import com.hk.board.service.HkServiceImp;
 
 @SpringBootTest
 class ApplicationTests {
-
+	
+	@Autowired
+	HkServiceImp hkServiceImp;
+	
 	@Test
 	void contextLoads() {
+		List<HkDto> list= hkServiceImp.getAllList();
+		System.out.println(list.size());
 	}
 
 }
