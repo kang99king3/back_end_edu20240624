f1a1e98a10e9f28bfba46b8c43a022cbece71cb1
bjkang <kang99king3@gmail.com>
2024-10-15 15:11:19 +0900

    15 임포트

---
+++ 15_schedule_springMVC_boot/src/main/java/com/hk/board/controller/CalController.java
@@ -0,0 +1,14 @@
+package com.hk.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalController {
+	
+	@GetMapping("/")
+	public String getMethodName() {
+		return "index";//  "/templates/index.html"
+	}
+	
+}
