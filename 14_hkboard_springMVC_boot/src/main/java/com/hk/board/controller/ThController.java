27e479ad69d16d3dd246691097a01c20296c5ab8
bjkang <kang99king3@gmail.com>
2024-10-15 10:45:53 +0900

    boardlist추가

--- 14_hkboard_springMVC_boot/src/main/java/com/hk/board/controller/ThController.java
+++ 14_hkboard_springMVC_boot/src/main/java/com/hk/board/controller/ThController.java
@@ -1,16 +1,36 @@
 package com.hk.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hk.board.dtos.HkDto;
+import com.hk.board.service.HkServiceImp;
+
 @Controller
-@RequestMapping(value = "/th")
+@RequestMapping(value = "/thboard")
 public class ThController {
 
+	@Autowired  // spring컨테이너가 hkServiceImp객체를 생성해서 주입해줌
+	private HkServiceImp hkServiceImp;
+	
+	//요청 url : localhost:9090/thboard/home 요청했을때 실행
 	@GetMapping("/home")
 	public String home() {
-		return new String();
+		
+		//thymeleaf 템플릿 리졸버가 동작함
+		return "thymeleaf/home";
+	}
+	
+	@GetMapping("/boardlist")
+	public String boardList(Model model) {
+		List<HkDto> list=hkServiceImp.getAllList();
+		model.addAttribute("list", list);
+		return "thymeleaf/boardlist";
 	}
 	
 }
