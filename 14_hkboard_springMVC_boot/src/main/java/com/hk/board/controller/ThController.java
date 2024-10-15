6851a73b8e142d3c615bd700295af423c5696f0a
bjkang <kang99king3@gmail.com>
2024-10-15 11:25:55 +0900

    글추가

--- 14_hkboard_springMVC_boot/src/main/java/com/hk/board/controller/ThController.java
+++ 14_hkboard_springMVC_boot/src/main/java/com/hk/board/controller/ThController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hk.board.dtos.HkDto;
@@ -33,6 +34,22 @@ public class ThController {
 		return "thymeleaf/boardlist";
 	}
 	
+	@GetMapping("/insertboardform")
+	public String insertboardform(Model model) {
+		
+		return "thymeleaf/insertboardform";
+	}
+	
+	@PostMapping("/insertboard")
+	public String insertboard(Model model,HkDto dto) {
+		System.out.println(dto);
+		boolean isS=hkServiceImp.insertBoard(dto);
+		if(isS) {
+			return "redirect:/thboard/boardlist";
+		}else {
+			return "error";
+		}
+	}
 }
 
 
