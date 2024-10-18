90a8fd7528f5ea179f298514165e7ad635a42e7e
bjkang <kang99king3@gmail.com>
2024-10-18 13:51:03 +0900

    추가

--- 15_schedule_springMVC_boot/src/main/java/com/hk/board/utils/Util.java
+++ 15_schedule_springMVC_boot/src/main/java/com/hk/board/utils/Util.java
@@ -1,5 +1,8 @@
 package com.hk.board.utils;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,4 +12,27 @@ public class Util {
 	public String isTwo(String str) {
 		return str.length()<2?"0"+str:str;// "5" -> "05"
 	}
+	
+	//mdate: 날짜형식으로 변환
+	public String toDates(String mdate) {
+		//Date형식으로 만들기: "yyyy-MM-dd hh:mm:ss"
+		//mdate: 12자리   --> "yyyy-MM-dd hh:mm:ss"
+		String m=mdate.substring(0, 4)+"-"
+				+mdate.substring(4, 6)+"-"
+				+mdate.substring(6, 8)+" "
+				+mdate.substring(8, 10)+":"
+				+mdate.substring(10)+":00";
+		Timestamp tm=Timestamp.valueOf(m);//String --> Date 변환
+		
+		//date타입의 값을 원하는 패턴으로 만들어서 문자열로 반환
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy년MM월dd일 HH:mm");
+		return sdf.format(tm);
+	}
 }
+
+
+
+
+
+
+
