1fb59a6e4e08ab1a0d5113164e69c69b47d79cc2
bjkang <kang99king3@gmail.com>
2024-10-30 15:03:09 +0900

    이미지 업로드경로 수정 WebMvcConfig.java, boardList.html

--- 18_login_board_springbootMVC/src/main/java/com/hk/board/config/WebMvcConfig.java
+++ 18_login_board_springbootMVC/src/main/java/com/hk/board/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.hk.board.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,12 +20,20 @@ public class WebMvcConfig implements WebMvcConfigurer{
 									,"/board/mulDel"
 									,"/board/boardUpdate"
 									,"/board/download"
-						            ,"/","/user/**","/css/**","/js/**");
+						            ,"/","/user/**","/css/**","/js/**"
+						            ,"/img/**","/upload/**");
 	
 //		registry.addInterceptor(new LoginInterceptor())
 //		.order(2)
 //		.addPathPatterns("/**")   //전체 요청에 대해 적용
 //		.excludePathPatterns("/board","/","/user/**","/css/**","/js/**");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("user.dir:"+System.getProperty("user.dir"));
+		registry.addResourceHandler("/upload/**")
+				.addResourceLocations("file:"+System.getProperty("user.dir")+"/src/main/webapp/upload/");
 	}
 }
 
