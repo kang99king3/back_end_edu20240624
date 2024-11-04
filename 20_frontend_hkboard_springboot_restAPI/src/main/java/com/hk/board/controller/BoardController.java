5c7917fdede86b193cedc9d6c3d2353569b7e08f
bjkang <kang99king3@gmail.com>
2024-11-04 16:45:12 +0900

    20_frontend_restAPI 임포트

---
+++ 20_frontend_hkboard_springboot_restAPI/src/main/java/com/hk/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.hk.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+	@GetMapping("/")
+	public String home() {
+		System.out.println("HOME 페이지입니다.");
+		return "home";
+	}
+	
+	@GetMapping("/board")
+	public String board() {
+		System.out.println("게시판 페이지입니다.");
+		return "board/boardList";
+	}
+}
+
+
+
+
+
